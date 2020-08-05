d70f457efb07c383b9c1bf8a5963bb828f7d194f
Sristi27 <sristi2705@gmail.com>
2020-08-05 22:40:28 +0530

    Workingcorrectly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,45 @@
-import { Component,OnDestroy,OnInit } from '@angular/core';
-import {CoronaService} from "./services/corona.service"
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CoronaService } from "./services/corona.service"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'covid-tracker';
-  countries=[];
+  countries = [];
+  country: any;
+  recovered:number=0;
+  deaths:number=0;
+  confirmed:number=0;
 
-  constructor(private service:CoronaService){};
+  constructor(private service: CoronaService) { };
 
-  ngOnInit(){
+  ngOnInit() {
 
     this.service.getCountries().subscribe(
-      data=>
-      {
-        this.countries=data;
+      data => {
+        this.countries = data;
+        this.countries.sort();
+        console.log(this.countries)
+        //console.log(data);
+      }
+    )
+  }
+
+  getCountry(country: any) {
+    this.country =country;
+  }
+  getData() {
+    this.service.getCountryData(this.country).subscribe(
+      data => {
         console.log(data)
+        var index=data.length-1;
+        this.confirmed=data[index].Confirmed;
+        this.recovered=data[index].Recovered;
+        this.deaths=data[index].Deaths;
+
       }
     )
   }
--- src/app/services/corona.service.ts
+++ src/app/services/corona.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class CoronaService {
 
-  constructor(public http:HttpClient) { }
+  constructor(public http: HttpClient) { }
 
-  getCountries(){
+  getCountries() {
 
     return this.http.get<any>("https://api.covid19api.com/countries");
-}
   }
-  
+  getCountryData(country:any):Observable<any>{
+
+    const url="https://api.covid19api.com/total/dayone/country/" + country;
+    return this.http.get<any>(url)
+  }
+}
+
